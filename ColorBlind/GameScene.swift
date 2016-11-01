35d08bfc2515e7249d3f4a0a454b0d264ea84af7
Sweeper777 <sumulang@gmail.com>
2016-11-01 07:52:52 +0000

    finally got the positions working

--- ColorBlind/GameScene.swift
+++ ColorBlind/GameScene.swift
@@ -2,10 +2,12 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        view.ignoresSiblingOrder = true
         let bg = self.childNode(withName: "bg") as! SKSpriteNode
         let node = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
         node.position = bg.convert(view.convert(CGPoint.zero, to: self), from: self)
         node.anchorPoint = CGPoint(x: 0, y: 1)
+        node.zPosition = 1000
         bg.addChild(node)
     }
 }
