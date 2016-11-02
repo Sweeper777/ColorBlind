d685af68990229b2e758a078e82732da9fb07f90
Sweeper777 <sumulang@gmail.com>
2016-11-02 07:19:48 +0000

    added Block class

---
+++ ColorBlind/Block.swift
@@ -0,0 +1,24 @@
+import SpriteKit
+
+class Block {
+    let node: SKSpriteNode
+    let colorCode: Int
+    let lane: Int
+    let gameSystem: GameSystem
+    
+    init(gameSystem: GameSystem, colorCode: Int, lane: Int) {
+        self.gameSystem = gameSystem
+        self.colorCode = colorCode
+        self.lane = lane
+        node = SKSpriteNode(imageNamed: "block")
+        node.position = gameSystem.lanePositions[lane]
+        let fall = SKAction.moveTo(y: gameSystem.fallToYPosition, duration: gameSystem.fallSpeed)
+        let onLanded = SKAction.run {
+            self.gameSystem.onLanded(self)
+        }
+        node.anchorPoint = CGPoint(x: 0.5, y: 0)
+        node.size = CGSize(width: gameSystem.laneWidth * 0.7, height: gameSystem.laneWidth * 0.7)
+        node.run(SKAction.colorize(with: gameSystem.colors[colorCode], colorBlendFactor: 0.7, duration: 0))
+        node.run(SKAction.sequence([fall, onLanded, SKAction.removeFromParent()]))
+    }
+}
