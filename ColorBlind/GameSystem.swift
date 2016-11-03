43d4eeb074bc58f5c14f7bd12ee7ea7610c14627
Sweeper777 <sumulang@gmail.com>
2016-11-03 07:08:52 +0000

    added new block algorithm

--- ColorBlind/GameSystem.swift
+++ ColorBlind/GameSystem.swift
@@ -44,5 +44,37 @@ class GameSystem {
         colors.append(UIColor(hue: complement, saturation: 1.0, brightness: 1.0, alpha: 1.0))
         self.colors = colors
         
+        for i in 0..<numberOfLanes {
+            collectors.append(Collector(gameSystem: self, lane: i))
+        }
+        
+        let newBlockClosure = {
+            var newBlocks = [(laneNumber: Int, colorCode: Int)]()
+            let randomNumber = Int.random(0, 9)
+            if randomNumber < 8 {
+                newBlocks.append((Int.random(0, self.numberOfLanes - 1), Int.random(0, self.colors.count - 1)))
+            }
+            var laneNum = Int.random(0, self.numberOfLanes - 1)
+            if randomNumber < 5 && !newBlocks.contains { $0.laneNumber == laneNum } {
+                newBlocks.append((laneNum, Int.random(0, self.colors.count - 1)))
+            }
+            laneNum = Int.random(0, self.numberOfLanes - 1)
+            if randomNumber < 2 && !newBlocks.contains { $0.laneNumber == laneNum } {
+                newBlocks.append((laneNum, Int.random(0, self.colors.count - 1)))
+            }
+            
+            let actionSequence = newBlocks.map({ tuple -> SKAction in
+                let newBlockAction = SKAction.run {
+                    _ = Block(gameSystem: self, colorCode: tuple.colorCode, lane: tuple.laneNumber)
+                }
+                let waitAction = SKAction.wait(forDuration: 0.7)
+                return SKAction.sequence([newBlockAction, waitAction])
+            })
+            scene.bg.run(SKAction.sequence(actionSequence))
+        }
+        
+        let waitAction = SKAction.wait(forDuration: 1.5)
+        let finalAction = SKAction.repeatForever(SKAction.sequence([waitAction, SKAction.run(newBlockClosure)]))
+        scene.bg.run(finalAction)
     }
 }
