bb321913a185b640b15558e3d1f137ef3ac8454b
Sweeper777 <sumulang@gmail.com>
2016-11-06 08:33:25 +0000

    added pause button

--- ColorBlind/GameScene.swift
+++ ColorBlind/GameScene.swift
@@ -1,10 +1,12 @@
 import SpriteKit
+import EZSwiftExtensions
 
 class GameScene: SKScene {
     var bg: SKSpriteNode!
     var gameSystem: GameSystem!
     var scoreLabel: SKLabelNode!
     var liveIndicator: SKSpriteNode!
+    var pauseButton: ButtonNode!
     
     override func didMove(to view: SKView) {
         view.ignoresSiblingOrder = true
@@ -25,6 +27,14 @@ class GameScene: SKScene {
         liveIndicator.zPosition = 999
         liveIndicator.anchorPoint = CGPoint(x: 1, y: 1)
         bg.addChild(liveIndicator)
+        
+        pauseButton = ButtonNode(imageNamed: "pauseButton")
+        let viewCoords = CGPoint(x: 10, y: 10)
+        pauseButton.position = self.bg.convert(view.convert(viewCoords, to: self), from: self)
+        pauseButton.size = CGSize(width: 100, height: 100)
+        pauseButton.anchorPoint = CGPoint(x: 0, y: 1)
+        pauseButton.zPosition = 999
+        self.bg.addChild(pauseButton)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,6 +44,26 @@ class GameScene: SKScene {
                     collector.colorCode = 0
                 } else {
                     collector.colorCode += 1
+                }
+            } else if let node = self.nodes(at: touch.location(in: self)).first as? ButtonNode {
+                let randomColor = UIColor(hue: CGFloat.random(), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+                node.run(SKAction.colorize(with: randomColor, colorBlendFactor: 0.7, duration: 0.1))
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if bg.isPaused {
+        } else {
+            pauseButton.run(SKAction.colorize(withColorBlendFactor: 0, duration: 0.1))
+            
+            for touch in touches {
+                if self.nodes(at: touch.location(in: self)).first == pauseButton {
+                    addChild(pauseScreen)
+                    pauseButton.removeAllActions()
+                    pauseButton.run(SKAction.colorize(withColorBlendFactor: 0, duration: 0))
+                    self.bg.isPaused = true
+                    pauseScreen.run(SKAction.fadeIn(withDuration: 0.3))
                 }
             }
         }
