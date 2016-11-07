8f0cb757b0efdcc73c40fe50f7887fe2f54dd066
Sweeper777 <sumulang@gmail.com>
2016-11-07 07:09:58 +0000

    added restart button

--- ColorBlind/GameOverScene.swift
+++ ColorBlind/GameOverScene.swift
@@ -14,5 +14,15 @@ class GameOverScene: SKScene {
         gameOverLabel.zPosition = 2001
         gameOverLabel.text = "GAME\nOVER"
         bg.addChild(gameOverLabel)
+        
+        viewCoords = CGPoint(x: self.view!.w / 2, y: self.view!.h / 2)
+        let bgCoords = bg.convert(self.view!.convert(viewCoords, to: self), from: self)
+        
+        let restartButton = ButtonNode(imageNamed: "restartButton")
+        restartButton.position = CGPoint(x: bgCoords.x + 220, y: bgCoords.y)
+        restartButton.size = CGSize(width: 200, height: 200)
+        restartButton.zPosition = 2001
+        restartButton.name = "restart"
+        bg.addChild(restartButton)
     }
 }
