f9031802fec6666cf9560c56d5bd46c61ab1585a
Sweeper777 <sumulang@gmail.com>
2016-11-07 07:11:47 +0000

    added BlockNode to fix retain cycles

--- ColorBlind/Block.swift
+++ ColorBlind/Block.swift
@@ -1,19 +1,21 @@
 import SpriteKit
 
 class Block {
-    let node: SKSpriteNode
+    weak var node: BlockNode!
     let colorCode: Int
     let lane: Int
-    let gameSystem: GameSystem
+    unowned var gameSystem: GameSystem
     
     init(gameSystem: GameSystem, colorCode: Int, lane: Int) {
         self.gameSystem = gameSystem
         self.colorCode = colorCode
         self.lane = lane
-        node = SKSpriteNode(imageNamed: "block")
+        let node = BlockNode(imageNamed: "block")
+        node.block = self
         node.position = gameSystem.blockPositions[lane]
         let fall = SKAction.moveTo(y: gameSystem.fallToYPosition, duration: gameSystem.fallSpeed)
         let onLanded = SKAction.run {
+            [unowned self] in
             self.gameSystem.onLanded(self)
         }
         node.anchorPoint = CGPoint(x: 0.5, y: 0)
@@ -21,6 +23,7 @@ class Block {
         node.zPosition = 1000
         node.run(SKAction.colorize(with: gameSystem.colors[colorCode], colorBlendFactor: 0.7, duration: 0))
         node.run(SKAction.sequence([fall, onLanded, SKAction.removeFromParent()]))
+        self.node = node
         gameSystem.scene?.bg.addChild(self.node)
     }
 }
---
+++ ColorBlind/BlockNode.swift
@@ -0,0 +1,6 @@
+import SpriteKit
+
+class BlockNode: SKSpriteNode {
+    var block: Block!
+    
+}
