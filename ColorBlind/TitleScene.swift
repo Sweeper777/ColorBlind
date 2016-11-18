836eaca5812a9ab73e2e03e58448e28b9aa7d5a5
Sweeper777 <sumulang@gmail.com>
2016-11-18 07:07:43 +0000

    added settings button, removed sound buttons

--- ColorBlind/TitleScene.swift
+++ ColorBlind/TitleScene.swift
@@ -5,8 +5,7 @@ import SwiftRandom
 class TitleScene: SKScene {
     var bg: SKSpriteNode!
     var startButton: ButtonNode!
-    var bgmButton: ButtonNode!
-    var soundEffectButton: ButtonNode!
+    var settingsButton: ButtonNode!
     
     override func didMove(to view: SKView) {
         bg = self.childNode(withName: "bg") as! SKSpriteNode
@@ -37,24 +36,14 @@ class TitleScene: SKScene {
         highscoreLabel.text = "Highcore: \(UserDefaults.standard.integer(forKey: "highscore"))"
         bg.addChild(highscoreLabel)
         
-        let bgmOn = !UserDefaults.standard.bool(forKey: "bgm")
-        bgmButton = ButtonNode(imageNamed: "bgm_\(bgmOn)")
-        bgmButton.zPosition = 1000
+        settingsButton = ButtonNode(imageNamed: "settingsButton")
+        settingsButton.zPosition = 1000
         viewCoords = CGPoint.zero
         let bgCoords = self.bg.convert(view.convert(viewCoords, to: self), from: self)
-        bgmButton.position = CGPoint(x: bgCoords.x + 30, y: bgCoords.y - 30)
-        bgmButton.anchorPoint = CGPoint(x: 0, y: 1)
-        bgmButton.size = CGSize(width: 100, height: 100)
-        bg.addChild(bgmButton)
-        
-        let soundEffectsOn = !UserDefaults.standard.bool(forKey: "soundEffects")
-        soundEffectButton = ButtonNode(imageNamed: "soundEffects_\(soundEffectsOn)")
-        soundEffectButton.zPosition = 1000
-        viewCoords = CGPoint.zero
-        soundEffectButton.position = CGPoint(x: bgmButton.position.x + bgmButton.frame.w + 30, y: bgmButton.position.y)
-        soundEffectButton.anchorPoint = CGPoint(x: 0, y: 1)
-        soundEffectButton.size = CGSize(width: 100, height: 100)
-        bg.addChild(soundEffectButton)
+        settingsButton.position = CGPoint(x: bgCoords.x + 30, y: bgCoords.y - 30)
+        settingsButton.anchorPoint = CGPoint(x: 0, y: 1)
+        settingsButton.size = CGSize(width: 100, height: 100)
+        bg.addChild(settingsButton)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -80,17 +69,13 @@ class TitleScene: SKScene {
                     view?.presentScene(scene, transition: transition)
                     return
                 }
-            } else if node == bgmButton {
-                let bgm = UserDefaults.standard.bool(forKey: "bgm")
-                UserDefaults.standard.set(!bgm, forKey: "bgm")
-                bgmButton.texture = SKTexture(imageNamed: "bgm_\(bgm)")
-                if let vc = (UIApplication.shared.delegate as? AppDelegate)?.window?.rootViewController as? GameViewController {
-                    vc.audioPlayer.volume = bgm ? 1.0 : 0.0
+            } else if node == settingsButton {
+                if let scene = SettingsScene(fileNamed: "SettingsScene") {
+                    scene.scaleMode = .aspectFill
+                    let transition = SKTransition.fade(withDuration: 0.5)
+                    view?.presentScene(scene, transition: transition)
+                    return
                 }
-            } else if node == soundEffectButton {
-                let soundEffects = UserDefaults.standard.bool(forKey: "soundEffects")
-                UserDefaults.standard.set(!soundEffects, forKey: "soundEffects")
-                soundEffectButton.texture = SKTexture(imageNamed: "soundEffects_\(soundEffects)")
             }
         }
         for child in bg.children {
